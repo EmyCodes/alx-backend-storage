d7a9a09b01003781f2871838d7dfbfb8ae2d3f46
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-13 23:29:15 +0100

    chore: modified 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,9 @@
--- Write a SQL script that creates a table users following these requirements
--- id: integer, never null, auto increment and primary key
--- email: string (255 characters), never null and unique
--- name: string (255 characters)
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-    );
+-- Creates a table users with attributes
+--	id: integer, never null, auto increment and primary key
+--	email: string(255 chars), never null and unique
+--	name: string(255 chars)
+CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+	);
