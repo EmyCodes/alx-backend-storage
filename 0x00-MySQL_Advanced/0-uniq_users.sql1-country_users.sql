b5c16c9920bedbca77e2a4119ca6e5c0253d9805
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-13 16:22:43 +0100

    chore: corrected typo mistake

--- 0x00-MySQL_Advanced/0-uniq_users.sql1-country_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql1-country_users.sql
@@ -1,2 +1,2 @@
 --- Write a SQL script that creates a table users following these requirements
-CREATE TABLE IF NOT EXISTS user (id int AUTO_INCREMENT, email VARCHAR(255), name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS users (id int AUTO_INCREMENT, email VARCHAR(255), name VARCHAR(255));
