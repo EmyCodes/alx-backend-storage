a000439fed244250ea4a4c107839430dc45a0722
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-13 16:41:31 +0100

    fix: syntax error being fixed

--- 0x00-MySQL_Advanced/0-uniq_users.sql1-country_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql1-country_users.sql
@@ -1,2 +1,6 @@
 --- Write a SQL script that creates a table users following these requirements
-CREATE TABLE IF NOT EXISTS users (id int AUTO_INCREMENT, email VARCHAR(255), name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
