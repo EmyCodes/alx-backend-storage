230a4a15b91b96297ed12ce254c73c66979abed5
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-13 18:12:02 +0100

    feat: added more columns

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -6,8 +6,8 @@
 --     country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id),
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM("US", "CO", "TN") DEFAULT "US" NOT NULL 
+    country ENUM("US", "CO", "TN") DEFAULT "US" NOT NULL,
+    PRIMARY KEY (id),
 );
